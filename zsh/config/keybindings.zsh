921ea226d96da901208c8966cb6b93d2a29e8fcf
Chris Fung <aergonaut@gmail.com>
2018-12-24 20:15:12 -0800

    add .zsh files

---
+++ zsh/config/keybindings.zsh
@@ -0,0 +1,22 @@
+# Copy the most recent command to the clipboard
+function _pbcopy_last_command(){
+  history | tail -1 | sed 's/ *[0-9]* *//' | pbcopy && \
+    tmux display-message "Previous command coppied to clipboard"
+}
+zle -N pbcopy-last-command _pbcopy_last_command
+bindkey '^x^y' pbcopy-last-command
+
+# Fuzzy match against history, edit selected value
+_uniqe_without_sort() { awk '!x[$0]++' }
+_fuzzy_history() {
+  zle -U "$(
+    history | \
+    tail -2000 | \
+    sed 's/ *[0-9]* *//' | \
+    _uniqe_without_sort | \
+    fzf-tmux --tac --reverse --no-sort
+  )"
+}
+zle -N fuzzy-history _fuzzy_history
+bindkey '^r' fuzzy-history
+
